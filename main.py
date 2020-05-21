0741d2f4610e08ff964005f83b3d79c3d1a1682d
shervinmo <abtin@riseup.net>
2020-05-22 03:52:25 +0430

    change config file from settings.py to .env

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import asyncio
+import os
 
 import asyncpg
 from sanic import Sanic
 
 from dictionary.urls import dic_bp_v1
-from settings import ACCESS_LOG, DB_DSN, DEBUG, HOST, PORT
 
-app = Sanic(__name__)
+app = Sanic("YATIM")
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/.env"
+os.environ.setdefault("YATIM_SETTINGS", dir_path)
+app.config.from_envvar('YATIM_SETTINGS')
 app.blueprint(dic_bp_v1)
 
 
 async def init_pool():
-    app.config["pool"] = await asyncpg.create_pool(DB_DSN)
+    app.config["pool"] = await asyncpg.create_pool(app.config["DB_DSN"])
 
 loop = asyncio.get_event_loop()
 app = loop.run_until_complete(init_pool())
 
 if __name__ == "__main__":
-    app.run(host=HOST, port=PORT, debug=DEBUG, access_log=ACCESS_LOG)
+    app.run()

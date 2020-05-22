6c73251b477668ea091291f45af0003f8078cae4
shervinmo <abtin@riseup.net>
2020-05-23 00:59:08 +0430

    fix env config

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from sanic import Sanic
 from dictionary.urls import dic_bp_v1
 
 app = Sanic("YATIM")
+
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/.env"
 os.environ.setdefault("YATIM_SETTINGS", dir_path)
 app.config.from_envvar('YATIM_SETTINGS')
+
 app.blueprint(dic_bp_v1)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host=app.config.get("HOST"),
+        port=app.config.get("PORT"),
+        access_log=app.config.get("ACCESS_LOG"),
+        debug=app.config.get("DEBUG"),
+    )
